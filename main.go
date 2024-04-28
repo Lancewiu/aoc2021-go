ec76e08d8fa0f03d24fa52059c5e8f97617ad332
Lawrence Niu <ranchwiu@gmail.com>
2024-04-27 21:28:27 -0400

    complete day 3 part 1

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"fmt"
 	"os"
-	"strconv"
 )
 
 const isTesting = false
@@ -12,9 +11,13 @@ const isTesting = false
 func main() {
 	filename := "input.txt"
 	newline := []byte("\n")
+	numbits := 12
+	bitmask := 0b111111111111
 	if isTesting {
 		filename = "test.txt"
 		newline = []byte("\r\n")
+		numbits = 5
+		bitmask = 0b11111
 	}
 
 	data, err := os.ReadFile(filename)
@@ -24,36 +27,36 @@ func main() {
 	}
 	lines := bytes.Split(data, newline)
 
-	var pos, depth, aim int
-	for iLine := 0; iLine < len(lines); iLine++ {
-		if 0 == len(lines[iLine]) {
+	onecounts := make([]int, numbits)
+	linecount := 0
+	for iline := 0; iline < len(lines); iline++ {
+		l := lines[iline]
+		if 0 == len(l) {
 			break
 		}
-		tokens := bytes.Split(lines[iLine], []byte(" "))
-		offset, err := strconv.Atoi(string(tokens[1]))
-		if nil != err {
-			fmt.Fprintf(os.Stderr, "error while converting from offset: %v\n", err)
+		linecount++
+		if numbits != len(l) {
+			fmt.Fprintf(
+				os.Stderr,
+				"assumed all lines have %d characters. got %d characters instead\n",
+				numbits,
+				len(l),
+			)
 			return
 		}
-		switch string(tokens[0]) {
-		case "forward":
-			pos += offset
-			depth += aim * offset
-			continue
-		case "down":
-			aim += offset
-			continue
-		case "up":
-			aim -= offset
-			continue
+		for ibit := 0; ibit < numbits; ibit++ {
+			if '1' == l[ibit] {
+				onecounts[ibit]++
+			}
 		}
-		fmt.Fprintf(
-			os.Stderr,
-			"error while parsing command string: unknown command string %s\n",
-			tokens[0],
-		)
-		return
 	}
-
-	fmt.Printf("horizontal position x final depth: %d\n", pos*depth)
+	var gammarate int
+	minority := linecount / 2
+	for ibit := 0; ibit < numbits; ibit++ {
+		if onecounts[ibit] > minority {
+			gammarate |= 1 << ((numbits - 1) - ibit)
+		}
+	}
+	epsilonrate := bitmask &^ gammarate
+	fmt.Printf("power consumption (gamma rate x epsilon rate): %d\n", gammarate*epsilonrate)
 }
